65d6d634c212b679b5cfe12e8fa7eb1fc961de15
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-01-15 15:32:42 -0600

    created handler for language changes to reset currentLetter and currentSub back to null on change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 
-export default function Navbar ({ setCurrentLanguage }) {
+export default function Navbar ({ handleLangChange }) {
 
     return (
         <div className="tenor-sans bg-gradient-to-r from-zinc-700 via-zinc-500 to-zinc-700 text-center text-zinc-100 p-1">
-            <Link key={1} to="/french" onClick={() => {setCurrentLanguage('french')}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
+            <Link key={1} to="/french" value="french" onClick={() => {handleLangChange()}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
             French
             </Link>
-            <Link key={2} to="/german" value="German" onClick={() => {setCurrentLanguage('german')}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
+            <Link key={2} to="/german" value="german" onClick={() => {handleLangChange()}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
             German
             </Link>
-            <Link key={3} to="/italian" value="Italian" onClick={() => {setCurrentLanguage('italian')}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
+            <Link key={3} to="/italian" value="italian" onClick={() => {handleLangChange()}} className="pt-2 pb-2 pr-4 pl-4 text-lg">
             Italian
             </Link>
         </div>
