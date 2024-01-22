4b6c1fbcce9ac92b9119b581122dd640c6f3bdad
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-01-22 15:24:33 -0600

    added welcome message on home page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 export default function Landing () {
-    <>
-    </>
+    return (
+        <h1 className="text-center text-lg p-3 font-bold">Welcome! Choose a language to get started.</h1>
+    )
 }
