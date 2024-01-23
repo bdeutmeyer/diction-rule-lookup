9a641162e21c2e40a0c90105d4f57a32830ecb77
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-01-22 20:14:22 -0600

    changed letter list spacing to look better on medium screens

--- src/components/LetterList.jsx
+++ src/components/LetterList.jsx
@@ -4,7 +4,7 @@ export default function LetterList({ handleLetterChange }) {
     return (
         <>
         {/* Displays alphabetical list when a language is in focus */}
-            <ul className='container-fluid flex flex-wrap row justify-between bg-zinc-700 px-3'>
+            <ul className='container-fluid flex flex-wrap row justify-center bg-zinc-700 px-3'>
                 {letters.map((l) => (
                     <li key={l.key} className='col bg-zinc-700'>
                         <button value={l.letter} onClick={handleLetterChange} className='text-zinc-100 btn'>
