339e785d82e64fd0bf7edfeac36c3ccd94788980
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-02-09 15:09:21 -0600

    added disclaimer paragraph to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,9 @@
-export default function Landing () {
+export default function Landing() {
     return (
-        <h2 className="text-center text-lg p-3 font-bold">Welcome! Choose a language to get started.</h2>
+        <>
+            <br />
+            <h2 className="text-center text-lg p-3 font-bold">Welcome! Choose a language to get started.</h2>
+            <p className="m-3 text-center">This site is meant as a quick-reference tool only, and I hope you find it handy! It is NOT a replacement for diction books, which contain a wealth of valuable information, including additional details, explanations, and examples. If you do not already own one, please purchase a diction book. You'll be doing yourself a favor by adding such a quality resource to your library, as well as supporting the scholars whose work is used as source material for this site (see source links in the footer below). Thank you!</p>
+        </>
     )
 }
